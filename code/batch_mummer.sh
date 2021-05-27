6e09f130b541bd70df21a41c352e433cb8ca987e
Siqi Li <siqili@rackham2.uppmax.uu.se>
2021-05-27 12:02:57 +0200

    updated mummerplot script

--- code/batch_mummer.sh
+++ code/batch_mummer.sh
@@ -13,4 +13,6 @@ module load MUMmer
 cd "/domus/h1/siqili/gen-ana/result/assembly-quality-assessment/MUMmerplot_result/"
 nucmer  -p nucmer  "/domus/h1/siqili/gen-ana/raw-data/durian-scaffold10-ncbi.fasta" "/domus/h1/siqili/gen-ana/result/correct-assembly/pilon.fasta"
 #mummerplot -Q /domus/h1/siqili/gen-ana/result/correct-assembly/pilon.fasta -f -l -t png  nucmer.delta
-mummerplot -c -f -t png  nucmer.delta
+delta-filter -1 nucmer.delta > nucmer_filtered.delta
+#mummerplot -c -f -t png  nucmer_filtered.delta
+mummerplot --large -l -t png  nucmer_filtered.delta
